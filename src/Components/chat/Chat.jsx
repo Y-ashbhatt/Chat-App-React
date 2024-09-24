912a2379a020745d8cdcbd697eb997558ce911f6
Yash Bhatt <0yashbhatt0@gmail.com>
2024-09-24 22:44:27 +0530

    fix

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -1,5 +1,22 @@
 import './chat.css'
 
 export default function Chat() {
-  return <div className='chat' >Chat</div>;
+  return <div className='chat' >
+    <div className="top">
+      <div className="user">
+        <img src="./avatar.png" alt="user-avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Lorem ipsum, dolor sit amet </p>
+        </div>
+      </div>
+      <div className="icons">
+        <img src="./phone.png" alt="phone-call" />
+        <img src="./video.png" alt="video-call" />
+        <img src="./info.png" alt="info" />
+      </div>
+    </div>
+    <div className="center"></div>
+    <div className="bottom"></div>
+  </div>;
 }
